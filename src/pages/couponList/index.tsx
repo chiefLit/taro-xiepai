0cddd4732006a9f4056dd819a362932dd4afbca3
chieflit <158385489@qq.com>
2019-11-11 21:35:12 +0800

    -

--- src/pages/couponList/index.tsx
+++ src/pages/couponList/index.tsx
@@ -1,5 +1,5 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, ScrollView } from '@tarojs/components'
 import { AtTabs, AtTabsPane } from 'taro-ui'
 import './index.less'
 
@@ -14,6 +14,11 @@ export default class CouponList extends Component {
    * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
+
+  constructor() {
+    super()
+  }
+
   config: Config = {
     navigationBarTitleText: '优惠券',
     enablePullDownRefresh: true
@@ -21,6 +26,16 @@ export default class CouponList extends Component {
 
   state = {
     current: 0,
+    page0: {
+      isActive: false,
+      params: {
+        status: 0,
+        currentPage: 1,
+        pageSize: 20
+      },
+      dataList: [],
+      pageInfo: {}
+    },
     page1: {
       isActive: false,
       params: {
@@ -40,21 +55,11 @@ export default class CouponList extends Component {
       },
       dataList: [],
       pageInfo: {}
-    },
-    page3: {
-      isActive: false,
-      params: {
-        status: 0,
-        currentPage: 1,
-        pageSize: 20
-      },
-      dataList: [],
-      pageInfo: {}
     }
   }
 
   componentWillMount() {
-    this.pullData(this.state.page1, 0)
+    this.pullData(this.state.page0, 0, null)
   }
 
   componentDidMount() { }
@@ -69,9 +74,9 @@ export default class CouponList extends Component {
    * 
    * @param page 当前页对象
    * @param index 页索引
-   * @param reStart 是否重新请求
+   * @param callBack 回调
    */
-  async pullData(page, index, callBack) {
+  async pullData(page: any, index: Number, callBack: any) {
     let data = await getCouponList(page.params)
     if (data.code !== 1) {
       Taro.showToast({
@@ -79,31 +84,18 @@ export default class CouponList extends Component {
         icon: 'none'
       })
     } else {
-      let dataList = [];
-      if (page.currentPage === 1) {
+      let dataList: Array<any> = [];
+      if (page.params.currentPage === 1) {
         dataList = [...data.object];
       } else {
         dataList = [...page.dataList, ...data.object];
       }
-      this.setState(preState => {
-        switch (index) {
-          case 0:
-            preState.page1.dataList = dataList;
-            preState.page1.pageInfo = data.page;
-            preState.page1.isActive = true;
-            break;
-          case 1:
-            preState.page2.dataList = dataList
-            preState.page2.pageInfo = data.page
-            preState.page2.isActive = true;
-            break;
-          case 2:
-            preState.page3.dataList = dataList
-            preState.page3.pageInfo = data.page
-            preState.page3.isActive = true;
-            break;
-          default:
-            break;
+      this.setState({
+        [`page${index}`]: {
+          ...page,
+          dataList,
+          pageInfo: data.page,
+          isActive: true
         }
       })
       callBack && callBack()
@@ -111,93 +103,102 @@ export default class CouponList extends Component {
   }
 
 
-  handleClick(value) {
-    this.setState(preState => {
-      preState.current = value
-      switch (value) {
-        case 0:
-          if (!preState.page1.isActive) {
-            preState.page1.currentPage = 1
-            this.pullData(preState.page1, value)
-          }
-          break;
-        case 1:
-          if (!preState.page2.isActive) {
-            preState.page2.currentPage = 1
-            this.pullData(preState.page2, value)
-          }
-          break;
-        case 2:
-          if (!preState.page3.isActive) {
-            preState.page3.currentPage = 1
-            this.pullData(preState.page3, value)
-          }
-          break;
-        default:
-          break;
+  handleClick(index: Number) {
+    let currPageObj = this.state[`page${index}`]
+    this.setState({
+      current: index,
+      [`page${index}`]: {
+        ...currPageObj,
+        params: {
+          ...currPageObj.params,
+          currentPage: 1
+        }
       }
     })
+    if (!currPageObj.isActive) {
+      this.pullData(currPageObj, index, null)
+    }
   }
 
   onPullDownRefresh() {
-    this.setState(preState => {
-      switch (preState.current) {
-        case 0:
-          preState.page1.currentPage = 1
-          this.pullData(preState.page1, preState.current, Taro.stopPullDownRefresh)
-          break;
-        case 1:
-          preState.page2.currentPage = 1
-          this.pullData(preState.page2, preState.current, Taro.stopPullDownRefresh)
-          break;
-        case 2:
-          preState.page3.currentPage = 1
-          this.pullData(preState.page3, preState.current, Taro.stopPullDownRefresh)
-          break;
-        default:
-          break;
+    let { current } = this.state
+    let currPageObj = this.state[`page${current}`]
+    this.setState({
+      [`page${current}`]: {
+        ...currPageObj,
+        params: {
+          ...currPageObj.params,
+          currentPage: 1
+        }
       }
     })
+    this.pullData(currPageObj, current, Taro.stopPullDownRefresh)
   }
 
-  randerItem(item) {
+  onReachBottom() {
+    let { current } = this.state
+    let currPageObj = this.state[`page${current}`]
+    if (currPageObj.params.currentPage < currPageObj.pageInfo.totalPages) {
+      this.setState({
+        [`page${current}`]: {
+          ...currPageObj,
+          params: {
+            ...currPageObj.params,
+            currentPage: currPageObj.params.currentPage++
+          }
+        }
+      })
+      this.pullData(currPageObj, current, null)
+    }
+  }
+
+  randerItem(item: any) {
     return (
       <View className="coupon-item">
         <View className="name">
-          <Text className="num">5</Text>
-          <Text className="unit"> 折</Text>
+          <Text className="num">{item.type === 1 ? item.discountRate : item.faceAmount}</Text>
+          <Text className="unit"> {item.type === 1 ? '折' : '%'}</Text>
         </View>
         <View className="info">
-          <View className="line1">仅限洗鞋订单</View>
-          <View className="line2">有效期至：2019-11-11 23：59</View>
+          <View className="line1">{item.name}</View>
+          <View className="line2">{item.describe}</View>
         </View>
         <View className="left-icon circle-icon"></View>
         <View className="right-icon circle-icon"></View>
-        <View className="state-icons"></View>
+        {item.status === 1 ? <View className="state-icons"></View> : null}
+        {item.status === 2 ? <View className="state-icons"></View> : null}
+        {/* <View className="state-icons"></View> */}
       </View>
     )
   }
 
   render() {
     const tabList = [{ title: '未使用' }, { title: '已使用' }, { title: '已失效' }]
-    let { current, page1, page2, page3 } = this.state;
+    let { current, page0, page1, page2 } = this.state;
     return (
       <View className='coupon-list-wrapper'>
         <AtTabs height="87" current={current} tabList={tabList} onClick={this.handleClick.bind(this)}>
           <AtTabsPane current={current} index={0} >
-            {
-              page1.dataList.map(ele => <View key={ele.id}> {this.randerItem(ele)} </View>)
-            }
+            <ScrollView className="ScrollView">
+              {
+                page0.dataList.map((ele: any) => <View key={ele.id}> {this.randerItem(ele)} </View>)
+              }
+            </ScrollView>
           </AtTabsPane>
           <AtTabsPane current={current} index={1}>
-            {
-              page2.dataList.map(ele => <View key={ele.id}> {this.randerItem(ele)} </View>)
-            }
+            <ScrollView className="ScrollView">
+              {
+                page1.dataList.map((ele: any) => <View key={ele.id}> {this.randerItem(ele)} </View>)
+              }
+            </ScrollView>
+
           </AtTabsPane>
           <AtTabsPane current={current} index={2}>
-            {
-              page3.dataList.map(ele => <View key={ele.id}> {this.randerItem(ele)} </View>)
-            }
+            <ScrollView className="ScrollView">
+              {
+                page2.dataList.map((ele: any) => <View key={ele.id}> {this.randerItem(ele)} </View>)
+              }
+            </ScrollView>
           </AtTabsPane>
         </AtTabs>
       </View>
